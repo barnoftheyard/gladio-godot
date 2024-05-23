8715792362ee07e59bb09f3d011497125db06763
brandon <taticalfacepalm1.a@gmail.com>
2024-05-23 05:33:37 -0700

    Updated VM

--- scripts/vm.gd
+++ scripts/vm.gd
@@ -4,24 +4,18 @@ extends EditorScript
 var memory: PackedByteArray
 
 var program = "
-AND R0, R0, 0                      ; clear R0
-LOOP                               ; label at the top of our loop
-ADD R0, R0, 1                      ; add 1 to R0 and store back in R0
-ADD R1, R0, -10                    ; subtract 10 from R0 and store back in R1
-BRn LOOP                           ; go back to LOOP if the result was negative"
+ADD Y 2
+GOTO LOOP
+ADD X 5
+MUL Y 2
+LOOP:
+ADD X 100"
 
-var registers = {
-	R0 = null,
-	R1 = null,
-	R2 = null,
-	R3 = null,
-	R4 = null,
-	R5 = null,
-	R6 = null,
-	R7 = null,
-	PC = null,
-	COND = null
-}
+var instructions = ["AND", "ADD", "SUB", "MUL", "DIV", "INC", "DEC", "GOTO"]
+
+var registers = {"X": 0, "Y": 0}
+
+var tags = {}
 
 enum flags {
 	FL_POS,
@@ -29,31 +23,95 @@ enum flags {
 	FL_NEG
 }
 
-func instruction_and(op1, op2, op3):
-	pass
+func math_operation(line, line_count, token_count, dest, operand1, operand2, operation):
+	if line[1] in registers:
+		dest = line[1]
+	else:
+		print("NOT VALID DESTINATION. BREAK AT " + str(line_count) + ", " + str(token_count))
+		return
+		
+	operand1 = registers[line[2]] if line[2] in registers else int(line[2])
+	
+	match operation:
+		"+":
+			registers[dest] += operand1
+		"-":
+			registers[dest] -= operand1
+		"*":
+			registers[dest] *= operand1
+		"/":
+			registers[dest] /= operand1
+		"++":
+			registers[dest] += 1
+		"--":
+			registers[dest] -= 1
 
+func tokenizer(input):
+	var program_lines = Array(input.split("\n"))
+	var index = 0
+	
+	var output = []
+	
+	for line in program_lines:
+		#remove comments
+		program_lines[index] = line.split(";", true, 1)[0]
+		program_lines[index] = program_lines[index].replace(",", "")
+		
+		var operands = Array(program_lines[index].split(" ", false, 0))
+		
+		output.append(operands)
+			
+		index += 1
+		
+	output.pop_front()
+	return output
+	
+	
+var line_count = 0
+var token_count = 0
+	
+func lexer(input, branch_point):
+	for line in input.slice(branch_point):
+		for token in line:
+			if token in instructions:
+				var dest = null
+				var operand1 = null
+				var operand2 = null
+				
+				match token:
+					"GOTO":
+						if line[1] in tags:
+							print(line[1])
+							lexer(tokenizer(program), int(tags[line[1]]))
+						else:
+							print("NOT VALID TAG. BREAK AT " + str(line_count) + ", " + str(token_count))
+							return
+					"ADD":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "+")
+					"SUB":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "-")
+					"MUL":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "*")
+					"DIV":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "/")
+					"INC":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "++")
+					"DEC":
+						math_operation(line, line_count, token_count, dest, operand1,
+						operand2, "--")
+					
+			token_count += 1
+		line_count += 1
+						
 # Called when the node enters the scene tree for the first time.
 func _run():
 	registers["COND"] = flags.FL_ZERO
 	memory.resize(65535)
 	
-	var program_lines = Array(program.split("\n"))
-	var index = 0
-	
-	for line in program_lines:
-		#remove comments
-		program_lines[index] = line.split(";", true, 1)[0]
-		
-		var operands = Array(program_lines[index].split(" ", false, 0))
-			
-		
-		#match operands[0]:
-			#"AND":
-				#print("AND FOUND")
-		
-		#print(operands)
-		index += 1
-	
-	program_lines.pop_front()
-	print(program_lines)
-
+	lexer(tokenizer(program), 0)
+	print(registers)
